025937d6db07b035eeb7cb758a62e84db9401e83
boahenisaac6-collab <boahenisaac6@gmail.com>
2026-05-16 06:55:45 +0000

    Add files via upload

--- app-config.js
+++ app-config.js
@@ -1,3 +1,3 @@
-// Isaac's Assessment Portal - Supabase configuration
+// Akuse Methodist SHTS Mathematics Assessment Portal - Supabase configuration
 window.AMEST_SUPABASE_URL = "https://qaizvskjnabmoejtqauf.supabase.co";
 window.AMEST_SUPABASE_ANON_KEY = "sb_publishable_a8jZmDVhiQJnWsSjYZgI6A_hPyE_6oB";
